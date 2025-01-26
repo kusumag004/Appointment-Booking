29a9cacfbe6c2d389184c6169819373a7788f4e8
Muskaan <muskan@example.com>
2025-01-26 11:56:12 +0530

    Home page

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Myprofile from './pages/MyProfile';
+import  Login  from './pages/Login'; // Named import
+import Appointment from './pages/Appointment';
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+<Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-profile' element={<Myprofile />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+       
+
+
+
+      </Routes>
+    
+    
+      
+    </div>
+  )
+}
+
+export default App
